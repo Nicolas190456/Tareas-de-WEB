77157642aa6a9daf481bbeadd53599ec80ca2b31
Nicolas <niquipte@gmail.com>
2022-07-25 11:01:30 -0500

    first commit

---
+++ Programacion web /main.js
@@ -0,0 +1,7 @@
+'use strict'
+//los contenedores que poseen JS 
+var variables=3; //Ambito global
+let variable2=4; //Ambito local
+const variable3=5; 
+console.log(hola bebesit0); 
+//alert("El valor de la variable 2: " + variable2)
